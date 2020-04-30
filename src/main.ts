4bda645241cd8c0b97757d15755caa226b4ea206
Asaju Enitan <kelvin.mai002@gmail.com>
2020-04-30 23:46:18 +0100

    Added to origins

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   });
   app.use(bodyParser.json({ limit: '50mb' }));
   app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-  await app.listen(process.env.PORT || '8080');
+  await app.listen(process.env.PORT || '3000');
 }
 bootstrap();
